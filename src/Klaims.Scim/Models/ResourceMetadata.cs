3ef181d01c1c34e56a01e0f8e1104a147e04e527
Alexey Anufriyev <aanufriyev@outlook.com>
2015-01-29 00:33:52 +0300

    Untracked files fix

---
+++ src/Klaims.Scim/Models/ResourceMetadata.cs
@@ -0,0 +1,31 @@
+﻿namespace Klaims.Scim.Models
+{
+	#region
+
+	using System;
+
+	#endregion
+
+	public class ResourceMetadata
+	{
+		public ResourceMetadata()
+		{
+		}
+
+		public ResourceMetadata(string resourceType, DateTime created, DateTime lastModified, int version)
+		{
+			ResourceType = resourceType;
+			Created = created;
+			LastModified = lastModified;
+			Version = version;
+		}
+
+		public string ResourceType { get; set; }
+
+		public int Version { get; set; }
+
+		public DateTime? Created { get; set; } = new DateTime();
+
+		public DateTime? LastModified { get; set; }
+	}
+}
