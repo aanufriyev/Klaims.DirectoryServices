c25abc0fc9cc1e1782a8ee4775076108abd9a328
Alexey Anufriyev <aanufriyev@outlook.com>
2015-02-07 23:54:29 +0300

    Untracked files(VS bug)

---
+++ src/Klaims.Scim/Query/ISearchQueryConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Klaims.Scim.Query
+{
+	using System;
+	using System.Linq.Expressions;
+
+	public interface ISearchQueryConverter<T> where T : class
+	{
+		Expression<Func<T, bool>> Convert(string filter, string sortBy, bool ascending, IAttributeNameMapper mapper = null);
+	}
+}
---
+++ src/Klaims.Scim/Rest/IUserResourceManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Klaims.Scim.Rest
+{
+	public interface IUserResourceManager<TUser> : IQueryableResourceManager<TUser>
+		where TUser : class
+	{
+		TUser CreateUser(TUser user, string password);
+
+		void ChangePassword(string id, string oldPassword, string newPassword);
+
+		TUser VerifyUser(string id, int version);
+	}
+}
