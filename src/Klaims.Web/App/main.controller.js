c09e4bf83c3ffc1a279c2292955a9f03d9c8c433
Alexey Anufriyev <aanufriyev@outlook.com>
2015-03-09 02:38:26 +0300

    Adding angular. Added Gulp. Build working with wwwroor folder for now.

---
+++ src/Klaims.Web/App/main.controller.js
@@ -0,0 +1,9 @@
+﻿(function() {
+	"use strict";
+
+	var MainController = function($scope) {
+		$scope.message = "dummy message to check everything in place";
+	};
+
+	angular.module("klaims").controller("MainController", ["$scope", MainController]);
+})();
---
+++ src/Klaims.Web/gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require("gulp"),
+    bower = require("gulp-bower");
+
+gulp.task("bower:install", function() {
+	bower({ cmd: "install", production: true })
+		.pipe(gulp.dest("wwwroot/lib/"));
+});
+
+// For now will just copy to output
+gulp.task("scripts", function() {
+	gulp.src("App/**/*.*")
+		.pipe(gulp.dest("wwwroot/app/"));
+});
+
+gulp.task('watch', function() {
+  gulp.watch("App/**/*.*", ['scripts']);
+  
+});
+
+gulp.task("default", ["watch","bower:install"]);
