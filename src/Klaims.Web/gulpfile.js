52f849726d97304b8634044e60a718d3aecb1c73
Alexey Anufriyev <aanufriyev@outlook.com>
2015-03-09 04:16:26 +0300

    Will revert to ST3 JS editing

--- src/Klaims.Web/gulpfile.js
+++ src/Klaims.Web/gulpfile.js
@@ -2,19 +2,21 @@
     bower = require("gulp-bower");
 
 gulp.task("bower:install", function() {
-	bower({ cmd: "install", production: true })
-		.pipe(gulp.dest("wwwroot/lib/"));
+    bower({
+            cmd: "install",
+            production: true
+        })
+        .pipe(gulp.dest("wwwroot/lib/"));
 });
 
 // For now will just copy to output
 gulp.task("scripts", function() {
-	gulp.src("App/**/*.*")
-		.pipe(gulp.dest("wwwroot/app/"));
+    gulp.src("App/**/*.*")
+        .pipe(gulp.dest("wwwroot/app/"));
 });
 
 gulp.task('watch', function() {
-  gulp.watch("App/**/*.*", ['scripts']);
-  
+    gulp.watch("App/**/*.*", ['scripts']);
 });
 
-gulp.task("default", ["watch","bower:install"]);
+gulp.task("default", ["watch", "bower:install"]);
